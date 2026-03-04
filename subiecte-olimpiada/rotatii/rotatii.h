a03b614cb67746c1557b13db2cafe159d038d8ea
“Bogdan <bogdan.daniel.ioan@outlook.com>
2026-03-04 20:35:47 +0200

    rotatii

--- subiecte-olimpiada/rotatii/rotatii.h
+++ subiecte-olimpiada/rotatii/rotatii.h
@@ -18,62 +18,89 @@ void assert_eq(int actualX, int actualY, int expectedX, int expectedY){
     }
 }
 
-void rezolvare(int C, int N, char tr[], int rot[], int expectedX = 0, int expectedY = 0) {
-    ifstream fin("D:/info/c++/clion/ProblemeDeConcurs/subiecte-olimpiada/rotatii/rotatii.in");
-    //ofstream fout("rotatii.out");
-    /*int C, N;
-    fin >> C >> N;
-    char tr[N + 1];
-    fin >> tr;*/
+void calc_pozitie(int cerinta, int numar_pasi, const char traseu[], const int rotatii[], int &rezultat_x, int &rezultat_y) {
+    rezultat_x = 0;
+    rezultat_y = 0;
 
-    if(C == 1) {
-        // int rot[N + 1];
-        int x = 0, y = 0;
-        for(int i = 0; i < N; i++) {
-            fin >> rot[i];
-            if(tr[i] == 'N') {
-                y = y + 1;
+    if (cerinta == 1) {
+        for (int i = 0; i < numar_pasi; i++) {
+            if (traseu[i] == 'N') {
+                rezultat_y = rezultat_y + 1;
+            } else if (traseu[i] == 'S') {
+                rezultat_y = rezultat_y - 1;
+            } else if (traseu[i] == 'E') {
+                rezultat_x = rezultat_x + 1;
+            } else if (traseu[i] == 'V') {
+                rezultat_x = rezultat_x - 1;
             }
-            else if(tr[i] == 'S') {
-                y = y - 1;
-            }
-            else if(tr[i] == 'E') {
-                x = x + 1;
-            }
-            else if(tr[i] == 'V') {
-                x = x - 1;
-            }
-            if(rot[i] == 90) {
-                int aux = x;
-                x = y;
-                y = -aux;
-            }
-            else if(rot[i] == 180) {
-                x = -x;
-                y = -y;
-            }
-            else if(rot[i] == 270) {
-                int aux = x;
-                x = -y;
-                y = aux;
+
+            if (rotatii[i] == 90) {
+                int aux = rezultat_x;
+                rezultat_x = rezultat_y;
+                rezultat_y = -aux;
+            } else if (rotatii[i] == 180) {
+                rezultat_x = -rezultat_x;
+                rezultat_y = -rezultat_y;
+            } else if (rotatii[i] == 270) {
+                int aux = rezultat_x;
+                rezultat_x = -rezultat_y;
+                rezultat_y = aux;
             }
         }
-        assert_eq(x,y,expectedX,expectedY);
-        //cout << x << " " << y << endl;  fout
+    }
+    // cerinta == 2 este lasata nerezolvata, conform enuntului initial.
+}
+
+void rezolvare(int cerinta, int numar_pasi, char traseu[], int rotatii[], int asteptat_x = 0, int asteptat_y = 0) {
+    int x_final = 0, y_final = 0;
+    calc_pozitie(cerinta, numar_pasi, traseu, rotatii, x_final, y_final);
+
+    if (asteptat_x != 0 || asteptat_y != 0) {
+        assert_eq(x_final, y_final, asteptat_x, asteptat_y);
+    } else {
+        cout << x_final << " " << y_final << endl;
+    }
+}
+
+void rezolvare() {
+    // Citire din fisier folosind calea relativa in depozit.
+    ifstream fin("subiecte-olimpiada/rotatii/rotatii.in");
+    if (!fin.is_open()) {
+        cerr << "Nu pot deschide fisierul subiecte-olimpiada/rotatii/rotatii.in" << endl;
+        return;
     }
 
-    if(C == 2) {
-        //todo
+    int cerinta, numar_pasi;
+    fin >> cerinta >> numar_pasi;
+
+    char *traseu = new char[numar_pasi + 1];
+    for (int i = 0; i < numar_pasi; i++) {
+        char pas;
+        fin >> pas;   // citeste urmatorul caracter (ignora spatiile)
+        traseu[i] = pas;
     }
+    traseu[numar_pasi] = '\0';
+
+    int *rotatii = new int[numar_pasi];
+    for (int i = 0; i < numar_pasi; i++) {
+        fin >> rotatii[i];
+    }
+
+    int x_final = 0, y_final = 0;
+    calc_pozitie(cerinta, numar_pasi, traseu, rotatii, x_final, y_final);
+    cout << x_final << " " << y_final << endl;
+
+    delete[] traseu;
+    delete[] rotatii;
 }
 
 
 void testare1() {
-    int C = 1;
-    int N = 4;
-    char tr[6] = "NNSV\0";
-    int rot[N] = {0,270,90,90};
-    int expectedX = 2, expectedY = 2;
-    rezolvare(C, N, tr, rot, expectedX, expectedY);
+    int cerinta = 1;
+    const int numar_pasi = 4;
+    char traseu[5] = "NNSV";
+    int rotatii[numar_pasi] = {0,270,90,90};
+    int asteptat_x = 2, asteptat_y = 2;
+    rezolvare(cerinta, numar_pasi, traseu, rotatii, asteptat_x, asteptat_y);
 }
 #endif //ROTATII_H
